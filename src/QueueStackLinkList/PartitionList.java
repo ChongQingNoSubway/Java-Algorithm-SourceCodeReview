961a1094e687cb1da20972305d1564d1b760ea3e
ChongQingNoSubway <zhuwenhiu@gmail.com>
2020-12-31 22:47:31 -0700

    partitionList

---
+++ src/QueueStackLinkList/PartitionList.java
@@ -0,0 +1,34 @@
+package QueueStackLinkList;
+
+public class PartitionList {
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode partition(ListNode head, int x) {
+        if(head == null) return null;
+
+        ListNode leftHead =  new ListNode(0);
+        ListNode rightHead = new ListNode(0);
+        ListNode right = rightHead;
+        ListNode left = leftHead;
+
+        while(head != null){
+            if(head.val < x){
+                left.next = head;
+                left = left.next;
+            }else{
+                right.next = head;
+                right = right.next;
+            }
+            head = head.next;
+        }
+        right.next = null;
+        left.next = rightHead.next;
+        return leftHead.next;
+    }
+}
